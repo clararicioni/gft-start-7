7bc5e20d2df93bbfda9b4f207a6d79f0cae17770
unknown <bergamoclara9@gmail.com>
2025-07-16 22:02:51 -0300

    :hotel: desafio hotel - explorando a linguagem c#

---
+++ trilha-net-explorando-desafio-main/Models/Reserva.cs
@@ -0,0 +1,60 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite == null)
+                throw new InvalidOperationException("Suite precisa ser cadastrada antes de adicionar hóspedes.");
+
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentException("A quantidade de hóspedes excede a capacidade da suíte.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            if (Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.90M;
+            }
+
+            return valor;
+        }
+    }
+}
